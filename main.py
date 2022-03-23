453d260316ff02a4384de75f993f0346afbde8a8
LogicalAlmond <logicalalmond@protonmail.com>
2022-03-23 18:18:04 -0400

    Added finished GUI

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from tkinter import *
 from turtle import back
 from webbrowser import BackgroundBrowser
@@ -5,8 +6,8 @@ from webbrowser import BackgroundBrowser
 # config root
 root = Tk()
 root.geometry('500x500')
-root.columnconfigure(1, weight=1)
-root.rowconfigure(5, weight=1)
+root.columnconfigure(1, weight=10)
+root.rowconfigure(5, weight=10)
 root.configure(background='#d7d6d5')
 
 #Receiver
@@ -27,9 +28,13 @@ sub_label.grid(row=2, column=0, sticky='we', padx=2, pady=2)
 sub_entry = Entry(root)
 sub_entry.grid(row=2,column=1, sticky='ew', padx=8, pady=3)
 
-# Button
+# Send Button
 send_button = Button(root, text='Send')
 send_button.grid(row=99, column=1, sticky='e', padx=3, pady=3, ipadx=2, ipady=2)
+
+# Attach Button
+attach_button = Button(root, text='Attach')
+attach_button.grid(row=99, column=0, columnspan=1, sticky='we', padx=3, pady=3, ipadx=2, ipady=2)
 
 #m Message
 message_label = Label(root, text='Message: ', font=("Arial", 14), background='#d7d6d5')
@@ -37,37 +42,5 @@ message_label.grid(row=4, column=0, sticky='wesn', padx=2, pady=2)
 message = Text(root)
 message.grid(row=5, column=0, columnspan=2, padx=8, pady=3)
 
-# mainFrame = Frame(root, width=500, height=500)
 
-# titleLabel = Label(root, text="Simple Email Client")
-# titleLabel.pack(side=TOP)
-
-# senderFrame = Frame(mainFrame)
-# ccFrame = Frame(mainFrame)
-# msgFrame = Frame(mainFrame)
-
-# senderLabel = Label(senderFrame, text='To: ')
-# senderEntry = Entry(senderFrame)
-
-# ccLabel = Label(ccFrame, text='CC: ')
-# ccEntry = Entry(ccFrame)
-
-# msgLabel = Label(msgFrame, text='Message: ',bd=0)
-# msgText = Text(msgFrame, bg="light yellow")
-
-
-
-# senderFrame.pack()
-# senderLabel.pack(side=LEFT)
-# senderEntry.pack(side=RIGHT)
-
-# ccFrame.pack()
-# ccLabel.pack(side=LEFT)
-# ccEntry.pack(side=RIGHT)
-
-# msgFrame.pack(padx=40, pady=40)
-# msgLabel.pack(side=TOP)
-# msgText.pack(side=BOTTOM, padx=1, pady=1)
-
-# mainFrame.pack()
 root.mainloop()
